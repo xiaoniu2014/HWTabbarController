e03b0b185fccdec9a68983dc798c15f4df4e0e45
xiaoniu2014 <116445168@qq.com>
2015-10-27 15:51:00 +0800

    Initial Commit

---
+++ HWTabbarController/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HWTabbarController
+//
+//  Created by hw on 15/10/27.
+//  Copyright © 2015年 hongw. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HWTabbarController/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HWTabbarController
+//
+//  Created by hw on 15/10/27.
+//  Copyright © 2015年 hongw. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
